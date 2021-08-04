2f958ee23ef43a997e4d2ae69d702478e2cfc721
SunNy820828449 <sunli11@baidu.com>
2021-08-04 11:34:35 +0000

    the definition of compiler

---
+++ paddle/fluid/compiler/piano/backends/llvm_ir/gpu/gpu_compiler.h
@@ -0,0 +1,35 @@
+// Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#pragma once
+
+#include "paddle/fluid/compiler/piano/backends/llvm_ir/llvm_compiler.h"
+
+namespace paddle {
+namespace piano {
+namespace gpu {
+
+class GpuCompiler : public LlvmCompiler {
+ public:
+  Schedules Apply(std::unique_ptr<NoteModule>&) override;
+
+ protected:
+  virtual void Optimize(std::unique_ptr<NoteModule>&) = 0;
+  void ConvertToIR(std::unique_ptr<NoteModule>&, std::unique_ptr<llvm::Module>&,
+                   Schedules&) override;
+  virtual void Compile(std::unique_ptr<llvm::Module>&, Schedules&) = 0;
+};
+
+}  // namespace gpu
+}  // namespace piano
+}  // namespace paddle
---
+++ paddle/fluid/compiler/piano/backends/llvm_ir/gpu/nvptx_compiler.h
@@ -0,0 +1,37 @@
+// Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#pragma once
+
+#include "paddle/fluid/compiler/piano/backends/llvm_ir/gpu/gpu_compiler.h"
+
+namespace paddle {
+namespace piano {
+namespace gpu {
+
+class NvptxCompiler : public GpuCompiler {
+ protected:
+  void Optimize(std::unique_ptr<NoteModule>&) override;
+  void Compile(std::unique_ptr<llvm::Module>&, Schedules&) override;
+
+  void InitNvptxContext();
+  void OptimizeLlvmIR(std::unique_ptr<llvm::Module>&);
+  std::string ConverToPtx(std::unique_ptr<llvm::Module>&);
+
+  std::string GetLlvmTarget();
+  std::string GetLlvmDataLayout();
+};
+
+}  // namespace gpu
+}  // namespace piano
+}  // namespace paddle
