f28df40385f9980be62d19a1b80d0648d6e23c3c
SunNy820828449 <sunli11@baidu.com>
2021-08-04 06:55:05 +0000

    align meta op

--- paddle/fluid/compiler/piano/backends/note_visitor_base.h
+++ paddle/fluid/compiler/piano/backends/note_visitor_base.h
@@ -16,61 +16,54 @@
 namespace paddle {
 namespace piano {
 
-// OpCode {
-//    AI API:  Convolution,  Dot,  BatchNormInference,  BatchNormTraining,
-//    BatchNormGrad,
-//    Unary Op:  Cast,  Log,  Exp,  Rsqrt,  Negative,  Sqrt,  Reverse,  Not,
-//    Binary Op:  Add,  Subtract,  Multiply,  Divide,  Minimum,  Maximum,
-//    Compare,  And,  Or,  Xor,
-//    Others Op:  Constant, Copy, Broadcast,  Reshape,  Rng,  Slice,
-//    Concatenate, Transpose,  Reduce,  Select,  Sort,  Tuple,
-//};
-
 template <typename InstructionPtr>
 class NoteVisitorBase {
  public:
   virtual ~NoteVisitorBase() {}
 
-  // AI api
-  virtual void VisitConvolution(InstructionPtr) = 0;
-  virtual void VisitDot(InstructionPtr) = 0;
-  virtual void VisitBatchNormTraining(InstructionPtr) = 0;
+  // Scalar op
+  virtual void VisitConstant(InstructionPtr) = 0;
+
+  //
   virtual void VisitBatchNormGrad(InstructionPtr) = 0;
   virtual void VisitBatchNormInference(InstructionPtr) = 0;
+  virtual void VisitBatchNormTraining(InstructionPtr) = 0;
+  virtual void VisitConvolution(InstructionPtr) = 0;
+  virtual void VisitDot(InstructionPtr) = 0;
 
   // Unary
+  virtual void VisitBroadcast(InstructionPtr) = 0;
   virtual void VisitCast(InstructionPtr) = 0;
+  virtual void VisitCopy(InstructionPtr) = 0;
   virtual void VisitExp(InstructionPtr) = 0;
   virtual void VisitLog(InstructionPtr) = 0;
   virtual void VisitNegative(InstructionPtr) = 0;
   virtual void VisitNot(InstructionPtr) = 0;
+  virtual void VisitReshape(InstructionPtr) = 0;
   virtual void VisitReverse(InstructionPtr) = 0;
-  virtual void VisitRsqrt(InstructionPtr) = 0;
+  virtual void VisitRsqrt(InstructionPtr) = virtual void VisitSlice(
+      InstructionPtr) = 0;
   virtual void VisitSqrt(InstructionPtr) = 0;
+  virtual void VisitTranspose(InstructionPtr) = 0;
 
   // Binary
   virtual void VisitAdd(InstructionPtr) = 0;
-  virtual void VisitSubtract(InstructionPtr) = 0;
-  virtual void VisitMultiply(InstructionPtr) = 0;
+  virtual void VisitAnd(InstructionPtr) = 0;
+  virtual void VisitCompare(InstructionPtr) = 0;
   virtual void VisitDivide(InstructionPtr) = 0;
   virtual void VisitMaximum(InstructionPtr) = 0;
   virtual void VisitMiniMum(InstructionPtr) = 0;
-  virtual void VisitCompare(InstructionPtr) = 0;
-  virtual void VisitAnd(InstructionPtr) = 0;
+  virtual void VisitMultiply(InstructionPtr) = 0;
   virtual void VisitOr(InstructionPtr) = 0;
+  virtual void VisitSubtract(InstructionPtr) = 0;
   virtual void VisitXor(InstructionPtr) = 0;
 
   // other
-  virtual void VisitBroadcast(InstructionPtr) = 0;
-  virtual void VisitConcatenate(InstructionPtr) = 0;
-  virtual void VisitConstant(InstructionPtr) = 0;
-  virtual void VisitCopy(InstructionPtr) = 0;
-  virtual void VisitReduce(InstructionPtr) = 0;
-  virtual void VisitReshape(InstructionPtr) = 0;
-  virtual void VisitRng(InstructionPtr) = 0;
   virtual void VisitSelect(InstructionPtr) = 0;
-  virtual void VisitSlice(InstructionPtr) = 0;
-  virtual void VisitTranspose(InstructionPtr) = 0;
+  virtual void VisitConcatenate(InstructionPtr) = 0;
+  virtual void VisitReduce(InstructionPtr) = 0;
+  virtual void VisitRng(InstructionPtr) = 0;
+  virtual void VisitSort(InstructionPtr) = 0;
   virtual void VisitTuple(InstructionPtr) = 0;
 };
 
